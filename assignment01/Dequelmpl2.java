bbb5d610649c69d1fddef336357e79af675fcfa7
KappaAnt <aalbane6@binghamton.edu>
2023-09-11 21:30:21 -0400

    Done

--- assignment01/Dequelmpl2.java
+++ assignment01/Dequelmpl2.java
@@ -1,5 +1,55 @@
 package CS442.CS442_P1.assignment01;
 
-public class Dequelmpl2<E> extends DList<E> implements Deque<E>{ //Class Adapter
-    
+public class Dequelmpl2<E> extends DLlist<E> implements Deque<E>{ //Class Adapter
+
+    //No need to instantiate a list here
+    public Dequelmpl2(){
+        /* */
+    }
+
+    public void addLast(E e){
+        insertList(e, length());
+    } 
+
+	public void addFirst(E e){
+        insertList(e, 0);
+    }
+
+	public E peek(){
+        if(length() == 0){return null;}
+
+        return get(0);
+    }
+	
+	public E poll(){
+        if(length() == 0){return null;}
+
+        E temp = get(0);
+        delete(0);
+
+        return temp;
+    }
+	
+	public E peekLast(){
+        if(length() == 0){return null;}
+
+        return get(length()-1);
+    }
+	
+	public E pollLast(){
+        if(length() == 0){return null;}
+
+        E temp = get(length()-1);
+        delete(length()-1);
+
+        return temp; 
+    }
+	
+	public int size(){
+        
+        return length();
+    }
+
+
+
 }
