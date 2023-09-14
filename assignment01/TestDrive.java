327ffd4445d510cf0bb46b87c542501e5c9442fd
Kyle Enriquez <kenriqu1@binghamton.edu>
2023-09-14 18:34:00 -0400

    Testing works for case, will test multiple cases

--- assignment01/TestDrive.java
+++ assignment01/TestDrive.java
@@ -3,8 +3,32 @@ package assignment01;
 public class TestDrive {
     public static void main(String[] args){
         DLlist<String> adaptee = new DLlist<String>();
-        Deque<String> adapter1 = new Dequelmpl1<>(adaptee);
-        Deque<String> adapter2 = new Dequelmpl2<>();
+        Deque<String> adaptar1 = new Dequelmpl1<>(adaptee); //object adapter
+        Deque<String> adaptar2 = new Dequelmpl2<>(); //class adapter
+
+        //object adaptar test
+        adaptar1.addLast("two");
+        adaptar1.addFirst("one");
+        adaptar1.addLast("three");
+        System.out.println(adaptar1.peekLast()); //should be 3
+        System.out.println(adaptar1.peek()); //should be 1
+        adaptar1.pollLast(); //remove last element
+        System.out.println(adaptar1.peekLast()); //should be 2
+        adaptar1.poll(); //remove first element
+        System.out.println(adaptar1.peek()); //should be 2
+
+
+        //class adaptar test
+        adaptar2.addLast("two");
+        adaptar2.addFirst("one");
+        adaptar2.addLast("three");
+        System.out.println(adaptar2.peekLast()); //should be 3
+        System.out.println(adaptar2.peek()); //should be 1
+        adaptar2.pollLast(); //remove last element
+        System.out.println(adaptar2.peekLast()); //should be 2
+        adaptar2.poll(); //remove first element
+        System.out.println(adaptar2.peek()); //should be 2
+
 
     }
 }
